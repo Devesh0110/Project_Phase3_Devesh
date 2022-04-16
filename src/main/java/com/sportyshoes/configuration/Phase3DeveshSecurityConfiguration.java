ad790b7bc80a90152dc847654550502412d43c8d
Devesh <deveshsingh0110@gmail.com>
2022-04-16 18:31:50 +0000

    Pushing Phase3 Project Devesh, added functionality

--- src/main/java/com/sportyshoes/configuration/Phase3DeveshSecurityConfiguration.java
+++ src/main/java/com/sportyshoes/configuration/Phase3DeveshSecurityConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SprotyShoesSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class Phase3DeveshSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/sportyshoes/controller/AdminController.java
+++ src/main/java/com/sportyshoes/controller/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -104,6 +105,16 @@ public class AdminController {
 		return new ResponseEntity<Product>(temp, HttpStatus.OK);
 
 	}
+	
+	@PutMapping("/products")
+	public String putProduct(@RequestBody Product product) {
+		Product temp = productService.putProduct(product);
+		if (temp == null) {
+			return "Product not updated";
+		}
+		return "Product Updated";
+
+	}
 
 	@GetMapping("/products/{productId}")
 	public ResponseEntity<Product> getProductById(@PathVariable("productId") int id) {
@@ -120,6 +131,16 @@ public class AdminController {
 		productService.deleteProductById(id);
 		return "Product with id="+id+" Deleted";
 	}
+//	
+//	@PutMapping("/products")
+//	public String updateOneProduct(@RequestBody Product product){
+//		// create a rest client
+//		RestTemplate restTemplate = new RestTemplate();
+//		restTemplate.put(PRODUCTS_URI, product);
+//		
+//		// collect and return response
+//		return "Product is updated successfully ";
+//	}
 
 	@GetMapping("/users")
 	public ResponseEntity<List<User>> getAllSignedUpUsers() {
